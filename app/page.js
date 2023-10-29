91029f02085b4cf9db86e3d7558cfe9aaa4b4574
SI <>
2023-10-29 09:19:20 +0900

    add: Top page read image ( not CSS yet)

--- app/page.js
+++ app/page.js
@@ -1,11 +1,42 @@
 // app/page.js
 import Link from "next/link";
+import Image from "next/image";
+import heroPic from "../public/images/index-hero.jpg";
+import profilePic from "../public/images/profile.jpg";
 
 const Index = () => {
   return (
     <>
-      <h1>Hello Next13!!!</h1>
-      <Link href="/contact">Move to Contact page</Link>
+      <div>
+        <Image src={heroPic} alt="hero" />
+        <div>
+          <h1>I'm Piro Garzarg</h1>
+          <h3>Next Developer</h3>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <h2>Next Nerd</h2>
+            <p>Lorem Ipsum is ****</p>
+          </div>
+          <div>
+            <Image src={profilePic} alt="hero" />
+          </div>
+        </div>
+        <div>
+          <h2>Skills</h2>
+          <div>
+            <div><img src = "/images/javascript.svg" alt="javascript"/><span>JavaScript / 3 years</span></div>
+            <div><img src = "/images/react.svg" alt="react"/><span>react / 2 years</span></div>
+            <div><img src = "/images/gatsby.svg" alt="gatsby"/><span>Gatsby / 0 years</span></div>
+            <div><img src = "/images/next.svg" alt="next"/><span>Next.JS / 2 years</span></div>
+          </div>
+        </div>
+        <div>
+          <Link href="/contact">Make It Happen!</Link>
+        </div>
+      </div>
     </>
   )
   };
